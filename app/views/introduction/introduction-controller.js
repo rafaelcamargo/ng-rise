17fca513df5d8dd7a520a8bbd3de5f626a7ca5cf
Rafael Camargo <contato@rafaelcamargo.com>
2015-06-09 20:53:22 -0300

    Renamed and reorganized app content files

---
+++ app/views/introduction/introduction-controller.js
@@ -0,0 +1,10 @@
+;(function(app){
+
+  app.controller('introductionController', ['$scope', function($scope){
+
+    $scope.title = 'ng-rise';
+    $scope.message = 'Well done, your angular base app is up and running. Enjoy!';
+
+  }]);
+
+})(app);
